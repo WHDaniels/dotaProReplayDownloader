c89f751593b47da8717507424bd6bec16fc7f57f
WHDaniels <51680292+WHDaniels@users.noreply.github.com>
2020-07-22 06:57:38 -0500

    Added capability to download to replays directory

--- OpenDotaParsing.py
+++ OpenDotaParsing.py
@@ -1,7 +1,15 @@
+import time
+
 import requests
 import json
 from urllib.parse import urlparse, urljoin
 from selenium import webdriver
+import os
+
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # These are some sample pro players whose games will be parsed for now
 # Gabbi: https://www.opendota.com/players/152545459
@@ -24,10 +32,9 @@ playerId = ["152545459", "97658618", "86745912", "105248644", "114619230",
 # the hero_id in their recent games (with "https://api.opendota.com/api/players/{account_id}/recentMatches")
 
 # And then get the {match_id} of the most recent games where hero id is {hero_you_want_to_practice}
-"""
+
 for id in playerId:
-    parseRecents = requests.get("https://api.opendota.com/api/players/" + id + "/recentMatches")
-    print("\n------------" + id + "------------\n")
+    parseRecents = requests.get("https://api.opendota.com/api/players/" + id + "/recentMatches", allow_redirects=True)
     with open(id + '.json', 'w') as file:
         json.dump(parseRecents.json(), file, sort_keys=True, indent='\t')
 
@@ -36,41 +43,60 @@ for id in playerId:
         for match in recentMatches:
             if match["hero_id"] == 1:
                 heroMatch = match["match_id"]
-                print(heroMatch)
-"""
+                matchUrl = "https://www.opendota.com/matches/" + str(heroMatch)
 
-heroMatch = "5524970375"
+                # the location of executable 'chromedriver' (C:\Users\mercm\PycharmProjects\OpenDotaStuff\chromedriver.exe)
+                # is added to path environment variable, removal or movement of this executable without updating the path
+                # will result in program errors
 
-# Parse the html of the opendota site to get the link to the replay of that match
+                # prepare the option for the chrome driver
+                options = webdriver.ChromeOptions()
+                options.add_argument('headless')
 
-matchUrl = "https://www.opendota.com/matches/" + heroMatch
+                # start chrome browser
+                browser = webdriver.Chrome(options=options)
+                browser.get(matchUrl)
 
-# the location of executable 'chromedriver' (C:\Users\mercm\PycharmProjects\OpenDotaStuff\chromedriver.exe)
-# is added to path environment variable, removal or movement of this executable without updating the path
-# will result in program errors
+                #delay = 5
+                #try:
+                #    myElem = WebDriverWait(browser, delay).until(
+                #        EC.presence_of_all_elements_located((By.XPATH, '//a[@href]')))
+                #    print("Loading complete!")
+                #except TimeoutException:
+                #    print("Loading took too much time!")
 
-# prepare the option for the chrome driver
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
+                time.sleep(7)
 
-# start chrome browser
-browser = webdriver.Chrome(options=options)
-browser.get(matchUrl)
+                # Parse the html of the opendota site to get the link to the replay of that match
+                elements = browser.find_elements_by_xpath('//a[@href]')
 
-elements = browser.find_elements_by_xpath("//a[@href]")
+                for a in elements:
+                    href = a.get_attribute("href")
+                    if href == "" or href is None:
+                        continue
+                    if "valve" in href:
+                        break
 
+                # Download the replay to the dota2 replays directory
+                if href.find('/'):
+                    fileName = href.rsplit('/', 1)[1]
+                fileName = fileName[0:-4]
 
-for a in elements:
-    href = a.get_attribute("href")
-    if href == "" or href is None:
-        continue
-    if "valve" in href:
-        break
-
-print(href)
-
-# Download the replay to the dota2 replays directory
+                completePath = "C:/Program Files (x86)/Steam/steamapps/common/dota 2 beta/game/dota/replays/"
+                if not os.path.exists(completePath):
+                    completePath = "E" + completePath[1:]
+                    if not os.path.exists(completePath):
+                        print("Replay directory not found. Your Dota 2 replays folder is either missing or "
+                              "is installed on a drive with a letter assignment of neither 'C:' nor 'E:'")
+                print(completePath + fileName)
+                print(matchUrl)
+                open(completePath + fileName, 'wb').write(parseRecents.content)
 
 # what can be added: 1) UI
 #                    2) automatic hero detection (find sufficiently high mmr players that
 #                       play a certain hero alot without manually finding them and parse those players)
+
+
+# need to fix:  1) substitute for 'time.sleep'
+#               2) delete jsons after they are made in project directory
+#               3) refactor into functions and clean up code
