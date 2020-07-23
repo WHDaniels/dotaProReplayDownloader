f2b64f913c160853a63462acbfb7dd723abe7546
WHDaniels <51680292+WHDaniels@users.noreply.github.com>
2020-07-23 05:23:23 -0500

    Added automatic Json deletion, cleaned code
    Added automatic json deletion functionality to reduce clutter in the progam directory. Also refactored and reformatted code with added comments.

--- OpenDotaParsing.py
+++ OpenDotaParsing.py
@@ -1,15 +1,104 @@
-import time
-
-import requests
-import json
-from urllib.parse import urlparse, urljoin
 from selenium import webdriver
+import requests
+import atexit
+import time
+import json
 import os
 
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
+def findMatchUrlAndExecuteRest(recents):
+    print("Creating json")
+    # writes a json file containing the players recent match data
+    with open(id + '.json', 'w') as file:
+        json.dump(recents.json(), file, sort_keys=True, indent='\t')
+
+    print("Reading recent matches from json")
+    # opens the json for reading
+    with open(id + '.json', 'r') as file:
+        recentMatches = json.load(file)
+
+    print("Finding specific hero matches\n")
+    # looks at every match and records the match url of the game they play the specified hero
+    # (since recentMatches is an array of json objects, it can be interpreted into python as a
+    # list of dictionaries so iterate through each object to find where the hero_id = specified hero
+    for match in recentMatches:
+        if match["hero_id"] == 1:
+            heroMatch = match["match_id"]
+            heroMatchUrl = "https://www.opendota.com/matches/" + str(heroMatch)
+            getReplay(heroMatchUrl)
+
+
+def getReplay(matchUrl):
+    # the location of executable 'chromedriver' in the program directory is essential for operation
+    print("Preparing the option for the chrome driver")
+    # prepare the option for the chrome driver
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+
+    print("Starting chrome browser")
+    # start the chrome browser
+    browser = webdriver.Chrome(executable_path=os.getcwd() + "\\chromedriver.exe", options=options)
+    browser.get(matchUrl)
+
+    print("Waiting for page to load...")
+    # give some time for the page to load before attempting to grab the href
+    time.sleep(1)
+
+    print("Getting replay link\n")
+    # Parse the html of the opendota site to get the link to the replay of the match
+    elements = browser.find_elements_by_xpath('//a[@href]')
+    for a in elements:
+        link = a.get_attribute("href")
+        if link == "" or link is None:
+            continue
+        if "valve" in link:
+            downloadReplay(link)
+
+
+def downloadReplay(replayLink):
+    print("Getting file name")
+    # get the file name through splitting the link and truncating
+    if replayLink.find('/'):
+        fileName = replayLink.rsplit('/', 1)[1]
+    fileName = fileName[0:-4]
+
+    print("Downloading replay to disk")
+    # specify the path to the dota 2 replays folder
+    completePath = "C:/Program Files (x86)/Steam/steamapps/common/dota 2 beta/game/dota/replays/"
+    if not os.path.exists(completePath):
+        completePath = "E" + completePath[1:]
+        if not os.path.exists(completePath):
+            print("Replay directory not found. Your Dota 2 replays folder is either missing or "
+                  "is installed on a drive with a letter assignment that is neither 'C:' nor 'E:'")
+
+    # Download the replay to that directory
+    open(completePath + fileName, 'wb').write(parseRecents.content)
+    print("Download Complete! You've downloaded to:")
+    print(completePath + fileName + "\n\n")
+
+
+# deletes the json (located in this programs directory) that contains the players recent games
+def deleteJsonOnFinish(id):
+    jsonPath = os.getcwd() + "\\" + id + ".json"
+    print("Deleting: " + jsonPath)
+    try:
+        os.remove(jsonPath)
+    except OSError as e:
+        print("Error: %s : %s" % (jsonPath, e.strerror))
+
+
+# deletes all json files in the program directory
+def deleteAllJsons():
+    filesInDirectory = os.listdir(os.getcwd())
+    jsonFiles = [file for file in filesInDirectory if file.endswith(".json")]
+    for file in jsonFiles:
+        jsonPath = os.getcwd() + "\\" + file;
+        print("Deleting: " + jsonPath)
+        os.remove(jsonPath)
+
+
+# executes deleteAllJsons() when program is exited
+atexit.register(deleteAllJsons)
 
 # These are some sample pro players whose games will be parsed for now
 # Gabbi: https://www.opendota.com/players/152545459
@@ -24,79 +113,20 @@ from selenium.webdriver.support import expected_conditions as EC
 # Mason: https://www.opendota.com/players/315657960
 # iLTW: https://www.opendota.com/players/113995822
 
-
 playerId = ["152545459", "97658618", "86745912", "105248644", "114619230",
             "175463659", "95430068", "132309493", "375507918", "315657960", "113995822"]
 
-# Parse all of their recent games to see if {hero_you_want_to_practice} matches
-# the hero_id in their recent games (with "https://api.opendota.com/api/players/{account_id}/recentMatches")
-
-# And then get the {match_id} of the most recent games where hero id is {hero_you_want_to_practice}
-
+# main loop
 for id in playerId:
+    print("\n--------------------------------------")
     parseRecents = requests.get("https://api.opendota.com/api/players/" + id + "/recentMatches", allow_redirects=True)
-    with open(id + '.json', 'w') as file:
-        json.dump(parseRecents.json(), file, sort_keys=True, indent='\t')
 
-    with open(id + '.json', 'r') as file:
-        recentMatches = json.load(file)
-        for match in recentMatches:
-            if match["hero_id"] == 1:
-                heroMatch = match["match_id"]
-                matchUrl = "https://www.opendota.com/matches/" + str(heroMatch)
+    findMatchUrlAndExecuteRest(parseRecents)
 
-                # the location of executable 'chromedriver' (C:\Users\mercm\PycharmProjects\OpenDotaStuff\chromedriver.exe)
-                # is added to path environment variable, removal or movement of this executable without updating the path
-                # will result in program errors
+    deleteJsonOnFinish(id)
 
-                # prepare the option for the chrome driver
-                options = webdriver.ChromeOptions()
-                options.add_argument('headless')
-
-                # start chrome browser
-                browser = webdriver.Chrome(options=options)
-                browser.get(matchUrl)
-
-                #delay = 5
-                #try:
-                #    myElem = WebDriverWait(browser, delay).until(
-                #        EC.presence_of_all_elements_located((By.XPATH, '//a[@href]')))
-                #    print("Loading complete!")
-                #except TimeoutException:
-                #    print("Loading took too much time!")
-
-                time.sleep(7)
-
-                # Parse the html of the opendota site to get the link to the replay of that match
-                elements = browser.find_elements_by_xpath('//a[@href]')
-
-                for a in elements:
-                    href = a.get_attribute("href")
-                    if href == "" or href is None:
-                        continue
-                    if "valve" in href:
-                        break
-
-                # Download the replay to the dota2 replays directory
-                if href.find('/'):
-                    fileName = href.rsplit('/', 1)[1]
-                fileName = fileName[0:-4]
-
-                completePath = "C:/Program Files (x86)/Steam/steamapps/common/dota 2 beta/game/dota/replays/"
-                if not os.path.exists(completePath):
-                    completePath = "E" + completePath[1:]
-                    if not os.path.exists(completePath):
-                        print("Replay directory not found. Your Dota 2 replays folder is either missing or "
-                              "is installed on a drive with a letter assignment of neither 'C:' nor 'E:'")
-                print(completePath + fileName)
-                print(matchUrl)
-                open(completePath + fileName, 'wb').write(parseRecents.content)
-
-# what can be added: 1) UI
-#                    2) automatic hero detection (find sufficiently high mmr players that
-#                       play a certain hero alot without manually finding them and parse those players)
-
-
-# need to fix:  1) substitute for 'time.sleep'
-#               2) delete jsons after they are made in project directory
-#               3) refactor into functions and clean up code
+"""
+To be added: 1) UI
+             2) automatic hero detection (find sufficiently high mmr players that play a certain hero
+                alot without manually finding them and parse those players' recent matches)
+"""
